8e966f9e22d557ec5d1d9ad2176be99d4982952b
Neto test <zilya.zakirova1991@gmail.com>
2023-05-11 14:11:04 +0300

    adds1

---
+++ src/main/java/ru/netology/Product.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import java.util.Objects;
+
+public class Product {
+    protected int id;
+    protected String title;
+    protected int price;
+
+    public Product(int id, String title, int price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+
+    // Вспомогательные методы для корректной работы equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && price == product.price && title.equals(product.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, price);
+    }
+    // геттеры + сеттеры
+    // на id только геттер — пусть будет неизменяемым для товара
+}
+
